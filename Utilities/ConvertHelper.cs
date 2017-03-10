a029df52532b7b398b56a9bd5953f22bc3d0aa76
zac <suoxd123@126.com>
2017-03-10 17:57:33 +0800

    fish

--- Utilities/ConvertHelper.cs
+++ Utilities/ConvertHelper.cs
@@ -109,6 +109,15 @@ namespace PcVedio
         /// 将byte[]转换成int
         /// </summary>
         /// <param name="data">需要转换成整数的byte数组</param>
+        public static int BytesToInt32(byte[] data)
+        {
+            return BytesToInt32(data, 0, true);
+        }
+
+        /// <summary>
+        /// 将byte[]转换成int
+        /// </summary>
+        /// <param name="data">需要转换成整数的byte数组</param>
         public static int BytesToInt32(byte[] data, bool lowBefore)
         {
             return BytesToInt32(data, 0, lowBefore);
@@ -122,30 +131,31 @@ namespace PcVedio
         /// <param name="start">起始位置</param>
         public static int BytesToInt32(byte[] data, int start, bool lowBefore)
         {
+            int byteCount = data.Length - start;
             //如果传入的字节数组长度小于4,则返回0
-            if (data.Length < 4)
+            if (byteCount <= 0)
             {
                 return 0;
+            }
+            else if(byteCount >= 4)
+            {
+                byteCount = 4;
             }
 
             //定义要返回的整数
             int num = 0;
 
-            //如果传入的字节数组长度大于4,需要进行处理
-            if (data.Length >= 4)
-            {
-                //创建一个临时缓冲区
-                byte[] tempBuffer = new byte[4];
+            //创建一个临时缓冲区
+            byte[] tempBuffer = new byte[4];
 
-                //将传入的字节数组的前4个字节复制到临时缓冲区
-                Buffer.BlockCopy(data, start, tempBuffer, 0, 4);
+            //将传入的字节数组的前4个字节复制到临时缓冲区
+            Buffer.BlockCopy(data, start, tempBuffer, 0, byteCount);
 
-                if (lowBefore)
-                    Array.Reverse(tempBuffer);
+            if (!lowBefore)
+                Array.Reverse(tempBuffer);
 
-                //将临时缓冲区的值转换成整数，并赋给num
-                num = BitConverter.ToInt32(tempBuffer, 0);
-            }
+            //将临时缓冲区的值转换成整数，并赋给num
+            num = BitConverter.ToInt32(tempBuffer, 0);
 
             //返回整数
             return num;
