887148af467f9a496d0cbc76c8d9b351a81dcfba
zac <suoxd123@126.com>
2017-03-18 00:17:20 +0800

    regex

---
+++ 正则表达式/StringValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Core
+{
+    public class StringValidator
+    {
+         public static bool CheckNumber(string strCheck)
+        {
+            if (!RegexHelper.IsValidNumber(strCheck))
+            {
+                MessageBox.Show("仅可输入整数");
+                return false;
+            }
+            return true;
+        }
+
+         public static bool CheckUnsignedNumber(string strCheck)
+         {
+             if (!RegexHelper.IsNumberUnsignedinteger(strCheck))
+             {
+                 MessageBox.Show("仅可输入正整数");
+                 return false;
+             }
+             return true;
+         }
+    }
+}
